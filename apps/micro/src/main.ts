dc9fd7befc9c90d701453b6861b2fd31fd40fc4d
gson Liang <yuanyu90221@gmail.com>
2022-12-27 00:02:50 +0800

    change createMicroservice to connectMicroservice

--- apps/micro/src/main.ts
+++ apps/micro/src/main.ts
@@ -7,14 +7,14 @@ const logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create(MicroModule);
   const configService = await app.get(ConfigService);
-  const micro = await NestFactory.createMicroservice(MicroModule, {
+  app.connectMicroservice({
     transport: Transport.TCP,
     options: {
       host: configService.get<string>('MICRO_SERVICE_HOST'),
       port: configService.get<number>('MICRO_SERVICE_PORT'),
     },
   });
-  micro.listen().then(() => {
+  app.startAllMicroservices().then(() => {
     logger.log('Microservice is listening...');
   });
 }
